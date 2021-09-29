951d4d522596f8425613fdd38c7889fc84a567d9
jimmy.pan <pan831225@gmail.com>
2021-09-29 18:58:53 +0800

    demo

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './style/reset.css' //reset
-import webp from '@/plugins/webp'
-
-const app = createApp(App)
-
-app.use(webp).mount('#app')
